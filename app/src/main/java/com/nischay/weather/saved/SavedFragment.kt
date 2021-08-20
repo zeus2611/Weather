a09866b681536870b99133998ba103ca2d5ae9af
Nischay <nischay26.scs@gmail.com>
2021-08-21 00:11:21 +0530

    Image Loading with Coil and Data Fetching from server using Retrofit2

---
+++ app/src/main/java/com/nischay/weather/saved/SavedFragment.kt
@@ -0,0 +1,26 @@
+package com.nischay.weather.saved
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.nischay.weather.R
+import com.nischay.weather.databinding.FragmentSavedBinding
+
+class SavedFragment : Fragment() {
+
+    private lateinit var binding: FragmentSavedBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_saved, container, false)
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
